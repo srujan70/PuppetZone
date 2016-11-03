fb938cf4068342b00f7e6b78448647606c456a44
shaikkhajaibrahim <shaik.khaja.ibrahim@live.in>
2016-11-03 05:56:29 +0530

    Added apache

---
+++ PuppetResources/Practice/apache.pp
@@ -0,0 +1,12 @@
+package { 'httpd':
+    ensure => installed,
+    
+}
+
+service { "httpd":
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+    # pattern    => 'httpd',
+}
